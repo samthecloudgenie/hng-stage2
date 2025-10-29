39fa0f78dc5e30351f5d5563a6dd142514dae4a1
samuel <samuelonah70@gmail.com>
2025-10-29 14:59:47 -0700

    added configs for script

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,22 +3,26 @@ set -e
 
 # Determine primary vs secondary based on ACTIVE_POOL
 if [ "${ACTIVE_POOL:-blue}" = "green" ]; then
-  export PRIMARY_HOST="${GREEN_HOST:-host.docker.internal}"
-  export PRIMARY_PORT="${GREEN_PORT:-8082}"
-  export SECONDARY_HOST="${BLUE_HOST:-host.docker.internal}"
-  export SECONDARY_PORT="${BLUE_PORT:-8081}"
+  export PRIMARY_HOST="${GREEN_HOST:-app_green}"
+  export PRIMARY_PORT="${GREEN_PORT:-3000}"
+  export SECONDARY_HOST="${BLUE_HOST:-app_blue}"
+  export SECONDARY_PORT="${BLUE_PORT:-3000}"
 else
-  export PRIMARY_HOST="${BLUE_HOST:-host.docker.internal}"
-  export PRIMARY_PORT="${BLUE_PORT:-8081}"
-  export SECONDARY_HOST="${GREEN_HOST:-host.docker.internal}"
-  export SECONDARY_PORT="${GREEN_PORT:-8082}"
+  export PRIMARY_HOST="${BLUE_HOST:-app_blue}"
+  export PRIMARY_PORT="${BLUE_PORT:-3000}"
+  export SECONDARY_HOST="${GREEN_HOST:-app_green}"
+  export SECONDARY_PORT="${GREEN_PORT:-3000}"
 fi
 
-# Render nginx config from template (include all env vars used)
-envsubst '${ACTIVE_POOL} ${BLUE_HOST} ${BLUE_PORT} ${GREEN_HOST} ${GREEN_PORT} \
-${PRIMARY_HOST} ${PRIMARY_PORT} ${SECONDARY_HOST} ${SECONDARY_PORT} \
+# Ensure default timeout values if not set
+export PROXY_CONNECT_TIMEOUT="${PROXY_CONNECT_TIMEOUT:-1s}"
+export PROXY_SEND_TIMEOUT="${PROXY_SEND_TIMEOUT:-5s}"
+export PROXY_READ_TIMEOUT="${PROXY_READ_TIMEOUT:-5s}"
+
+# Render Nginx configuration from template
+envsubst '${PRIMARY_HOST} ${PRIMARY_PORT} ${SECONDARY_HOST} ${SECONDARY_PORT} \
 ${PROXY_CONNECT_TIMEOUT} ${PROXY_READ_TIMEOUT} ${PROXY_SEND_TIMEOUT}' \
   < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Start nginx in foreground
+# Start nginx
 nginx -g "daemon off;"
